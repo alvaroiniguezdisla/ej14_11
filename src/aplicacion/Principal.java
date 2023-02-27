6286f50da22fb670e767e43679b64ffd05395fd7
Álvaro <ainiguezdislaa@gmail.com>
2023-02-27 11:28:20 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,16 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import dominio.ej14_11;
+public class Principal {
+    public static void main(String[] args) {
+        List<String> al = new ArrayList<String>();
+        al.add("A");
+        al.add("B");
+        al.add("C");
+        al.add("Hola mundo");
+        al.add("alvaro");
+        System.out.println(ej14_11.noEmpiezaPorA(al));
+    }
 }
--- src/dominio/ej14_11.java
+++ src/dominio/ej14_11.java
@@ -1,2 +1,12 @@
-package dominio;public class ej14_11 {
+package dominio;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import java.util.stream.Collectors;
+
+public class ej14_11 {
+    public static List<String> noEmpiezaPorA(List<String> al){
+        return Stream.iterate(0,i->i+1).limit(al.size()).map(i->al.get(i)).filter(i->!i.startsWith("A")).collect(Collectors.toCollection(ArrayList::new));
+    }
 }
